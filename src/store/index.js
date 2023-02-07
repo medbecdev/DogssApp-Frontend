fc6ef17d0a5df8746de32a08690691ce7eb281a0
mederocc <mederocc@gmail.com>
2023-02-07 13:50:13 -0300

    >

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducer";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
